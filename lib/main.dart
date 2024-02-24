088ea8c95201937613f9c5a6bd0f3c718a1a956a
João Sereia <joao.lo.sereia@gmail.com>
2024-02-24 01:40:12 -0300

    add app builder

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:tabnews/core/bindings/initial_binding.dart';
 import 'package:tabnews/core/routes/app_pages.dart';
 import 'package:tabnews/core/routes/app_routes.dart';
 import 'package:tabnews/core/theme/app_theme.dart';
+import 'package:tabnews/core/theme/extensions/theme_colors_extension.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -36,6 +37,13 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.dark,
       debugShowCheckedModeBanner: false,
       locale: const Locale('pt', 'BR'),
+      builder: (context, child) {
+        final colors = Theme.of(context).extension<ThemeColors>()!;
+        return ColoredBox(
+          color: colors.background,
+          child: child,
+        );
+      },
     );
   }
 }
