b9200cea8c8692899b315995943a7beaafc299c1
João Sereia <joao.lo.sereia@gmail.com>
2024-09-01 02:17:00 -0300

    feat(theme/theme_colors): add opacity to the surface color in the modern color scheme (light/dark)

--- lib/core/theme/theme_colors.dart
+++ lib/core/theme/theme_colors.dart
@@ -5,7 +5,7 @@ final class ThemeLightColors extends ThemeColors {
   const ThemeLightColors({
     super.primary = const Color(0xFF0969da),
     super.onPrimary = const Color(0xFFFFFFFF),
-    super.secondary = const Color(0xFFFFFFFF),
+    super.secondary = const Color.fromRGBO(255, 255, 255, 0.8),
     super.onSecondary = const Color(0xFF1F2328),
     super.tertiary = const Color(0xFFF6F8FA),
     super.onTertiary = const Color(0xFF24292F),
@@ -28,7 +28,7 @@ final class ThemeDarkColors extends ThemeColors {
   const ThemeDarkColors({
     super.primary = const Color(0xFF2F81F7),
     super.onPrimary = const Color(0xFFE6EDF3),
-    super.secondary = const Color(0xFF0D1117),
+    super.secondary = const Color.fromRGBO(13, 17, 23, 0.8),
     super.onSecondary = const Color(0xFFE6EDF3),
     super.tertiary = const Color(0xFF21262D),
     super.onTertiary = const Color(0xFFC9D1D9),
