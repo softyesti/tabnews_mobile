5894fc67d09c03b8159580644df446fe639f131e
João Sereia <joao.lo.sereia@gmail.com>
2024-09-01 04:29:20 -0300

    feat(widgets): add safe area widget

---
+++ lib/core/widgets/safe_area_widget.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:tabnews/core/mixin/theme_mixin.dart';
+
+class SafeAreaWidget extends StatelessWidget with ThemeMixin {
+  const SafeAreaWidget({
+    required this.child,
+    this.top = false,
+    this.bottom = false,
+    this.left = false,
+    this.right = false,
+    super.key,
+  });
+
+  final bool top;
+  final bool bottom;
+  final bool left;
+  final bool right;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final metrics = getMetrics();
+
+    var minimum = EdgeInsets.zero;
+    if (Platform.isAndroid) {
+      minimum = minimum.copyWith(
+        top: top ? metrics.medium : 0,
+        bottom: bottom ? metrics.medium : 0,
+        left: left ? metrics.medium : 0,
+        right: right ? metrics.medium : 0,
+      );
+    }
+
+    return SafeArea(
+      top: top,
+      bottom: bottom,
+      left: left,
+      right: right,
+      minimum: minimum,
+      child: child,
+    );
+  }
+}
